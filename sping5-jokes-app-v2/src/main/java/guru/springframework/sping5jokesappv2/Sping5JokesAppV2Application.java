ee94d006a6b68d7265dd7dac8367fa1dbb962aae
ziskle20 <ziskle20@SHS5099433C.cable.rcn.com>
2022-02-21 21:23:14 -0600

    Create Spring Boot project Closed #1

---
+++ sping5-jokes-app-v2/src/main/java/guru/springframework/sping5jokesappv2/Sping5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sping5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sping5JokesAppV2Application.class, args);
+	}
+
+}
---
+++ sping5-jokes-app-v2/src/test/java/guru/springframework/sping5jokesappv2/Sping5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sping5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
